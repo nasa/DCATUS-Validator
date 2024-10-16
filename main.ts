069d93e924adfe55f7da972f4f0e0e3d990aa6e3
Rendon, Daniel C. (JSC-IO111)[MORI ASSOCIATES INC] <daniel.c.rendon@nasa.gov>
2024-10-16 14:53:35 -0500

    init convert to deno

---
+++ main.ts
@@ -0,0 +1,23 @@
+import { validateFile } from "./schema-validator.ts";
+
+const inputFile = Deno.args[0];
+const outputFile = `validation-errors.json`;
+
+if (!inputFile) {
+    console.error("Please provide an input file path.");
+    Deno.exit(1);
+}
+
+try {
+    const results = await validateFile(inputFile);
+    results.valid ? console.log("Validation succeeded. The file is valid according to the schema.") : console.log("Validation failed. The file is not valid according to the schema.");
+    const outputJSON = JSON.stringify(results, null, 2);
+    await Deno.writeTextFile(outputFile, outputJSON);
+    console.log(`Validation results written to ${outputFile}`);
+} catch (error: unknown) {
+    if (error instanceof Error) {
+        throw new Error(`Validation error: ${error.message}`);
+    } else {
+        throw new Error("An unknown error occurred during validation");
+    }
+}
---
+++ schema-validator.ts
@@ -0,0 +1,80 @@
+import Ajv, { ErrorObject } from "npm:ajv@8.17.1";
+import addFormats from "npm:ajv-formats@3.0.1";
+import schemaFile from "./schemas/dcat-us-1.json" with { type: "json" };
+
+interface ValidationResponse {
+  valid: boolean;
+  amount: number;
+  errors: ValidationError[] | [];
+}
+interface ValidationError {
+  dataPath: string;
+  message?: string;
+  datasetIdentifier?: string;
+}
+
+export const validateFile = async (
+  inputFile: string,
+): Promise<ValidationResponse> => {
+  try {
+    const data = JSON.parse(await Deno.readTextFile(inputFile));
+    // @ts-ignore: Suppressing "This expression is not constructable" error
+    const ajv = new Ajv({ allErrors: true });
+
+    // @ts-ignore: Suppressing "This expression is not callable" error
+    addFormats(ajv);
+    ajv.addFormat("doi-uri", {
+      type: "string",
+      validate: (str: string): boolean => {
+        try {
+          const schemeRegex = /^(https?):\/\//i;
+          const decoded = decodeURIComponent(str);
+
+          if (!schemeRegex.test(str)) {
+            return false;
+          }
+
+          encodeURI(decoded);
+          return true;
+        } catch (_e) {
+          return false;
+        }
+      },
+    });
+
+    const validate = ajv.compile(schemaFile);
+    const valid = validate(data);
+
+    if (!valid) {
+      const errors = validate.errors.map((err: ErrorObject) => {
+        const newErr: ValidationError = {
+          dataPath: err.instancePath,
+          message: err.message?.replaceAll('"', ""),
+        };
+        let datasetIdentifier = null;
+        if (err.instancePath.includes("/dataset")) {
+          datasetIdentifier =
+            data.dataset[err.instancePath.split("/")[2]].identifier;
+        }
+        if (datasetIdentifier) {
+          newErr.datasetIdentifier = datasetIdentifier;
+        }
+        return newErr;
+      });
+
+      return {
+        valid: false,
+        amount: errors.length,
+        errors: errors,
+      };
+    }
+
+    return {
+      valid: true,
+      amount: 0,
+      errors: [],
+    };
+  } catch (error) {
+    throw error;
+  }
+};
