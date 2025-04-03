d761df67fa47b54e754fd5961a63d7bebccb49dd
Daniel C. Rendon <daniel.c.rendon@nasa.gov>
2025-04-03 14:29:16 -0500

    quick refactor

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 
 # DCAT-US JSON Schema Validation Script
 
-This script validates a JSON file against the [DCAT-US Schema](https://resources.data.gov/resources/dcat-us/) (v1.1) and outputs any validation errors in a `validation-errors.json` file.
+This script validates a JSON file against the [DCAT-US Schema](https://resources.data.gov/resources/dcat-us/) (v1.1) and outputs any validation errors in a `validation-results.json` file.
 
 ## Usage
 
@@ -20,28 +20,42 @@ node dcat-validator.js {input-file-path}
 Example:
 
 ```bash
-node dcat-validator.js ./data/dcat-file.json
+node dcat-validator.js ./test-json/dcat1-sample.json
 ```
 
 ## Output
 
-- **If the file is valid**: A success message will be logged, and no errors will be written.
-- **If the file is invalid**: The script will log the errors, and a `validation-errors.json` file will be generated containing the following structure:
+- **If the file is valid**: A success message will be logged, and a `validation-results.json` file will be generated containing the following structure.
+```json
+{
+  "isValid": true,
+  "amount": 0,
+  "errors": []
+}
+```
 
+
+- **If the file is invalid**: The script will log the errors, and a `validation-results.json` file will be generated containing the following structure:
 ```json
 {
   "isValid": false,
   "amount": "<number_of_errors>",
   "errors": [
     {
-      "dataPath": "<path_to_invalid_data>",
+      "datasetTitle": "<name_of_the_dataset>",
+      "datasetId": "<dataset_id>",
+      "dataPath": "<path_to_the_error>", // This is the path in the input JSON file where the error occurred.
+      "keyword": "<error_keyword>",
       "message": "<error_message>",
-      "datasetIdentifier": "<dataset_identifier_if_applicable>"
+      "schemaPath": "<path_to_schema_where_error_occurred>", // This is the path in the schema where that flagged the error.
     }
   ]
 }
 ``` 
+**Error Notes**:
+- If a dataset has multiple errors, each error will be listed separately in the `errors` array.
+- If a dataset is breaking multiple rules for a single field(ex: bureauCode), each rule will be listed separately in the `errors` array.
+  - Fixing the dataset to adhere to one rule will clear all the errors for that field.
 
 ### Notes
 - Updated [GSA DCAT-US Schema](https://github.com/GSA/ckanext-datajson/tree/main/ckanext/datajson/pod_schema/federal-v1.1) from draft-04 to draft-07 to keep inline with GSA's schema.
-- Did not keep validation for "REDACTED" values.
--- dcat-validator.js
+++ dcat-validator.js
@@ -1,10 +1,9 @@
 import fs from "fs/promises";
 import Ajv from "ajv";
-import addFormats from "ajv-formats";
 
 const inputFile = process.argv[2];
-const outputFile = `validation-errors.json`;
-const DCAT_US_SCHEMA = JSON.parse(await fs.readFile("./schemas/dcat-us-1.json", "utf8"))
+const outputFile = `validation-results.json`;
+const DCAT_US_SCHEMA = JSON.parse(await fs.readFile("./schemas/gsa-dcat.json", "utf8"))
 
 if (!inputFile) {
   console.error("Please provide an input file path.");
@@ -15,51 +14,37 @@ const validateDcatFile = async (inputFile) => {
   try {
     const fileContent = await fs.readFile(inputFile, "utf8");
     const data = JSON.parse(fileContent);
-    const ajv = new Ajv({ allErrors: true });
 
-    addFormats(ajv);
-    ajv.addFormat("doi-uri", {
-      type: "string",
-      validate: (str) => {
-        try {
-          const schemeRegex = /^(https?):\/\//i;
-          const decoded = decodeURIComponent(str);
-
-          if (!schemeRegex.test(str)) {
-            return false;
-          }
-
-          encodeURI(decoded);
-          return true;
-        } catch (_e) {
-          return false;
-        }
-      },
-    });
-    
+    const ajv = new Ajv();
     const validate = ajv.compile(DCAT_US_SCHEMA)
-    const valid = validate(data);
+    let results = []
+    // const valid = validate(data);
 
-    if(!valid){
+    for (let i = 0; i < data.dataset.length; i++) {
+      const dataset = data.dataset[i];
+      const valid = validate(dataset);
+      if (!valid) {
+        validate.errors.map((err) => {
+          const error = {
+            datasetId: dataset.identifier,
+            datasetTitle: dataset.title,
+            dataPath: `dataset[${i}]${err.dataPath}`,
+            keyword: err.keyword,
+            message: err.message,
+            schemaPath: err.schemaPath,
+            // params: err.params,
+          }
+          results.push(error)
+        });
+      }
+    }
+
+    if(results.length){
       console.log("File failed validation.");
-      const errors = validate.errors.map((err) => {
-        const newErr = {
-          dataPath: err.instancePath,
-          message: err.message.replaceAll("\"", ""),
-        }
-        let datasetIdentifier = null;
-        if (err.instancePath.includes("/dataset")){
-          datasetIdentifier = data.dataset[err.instancePath.split("/")[2]].identifier;
-        }
-        if (datasetIdentifier) {
-          newErr.datasetIdentifier = datasetIdentifier
-        }
-        return newErr;
-      });
       return {
         isValid: false,
-        amount: errors.length,
-        errors: errors
+        amount: results.length,
+        errors: results
       };
     }
 
