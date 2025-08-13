83402fbec53831b93013055f6ac10b13b9ab9a8b
Daniel C. Rendon <daniel.c.rendon@nasa.gov>
2025-08-13 09:38:31 -0500

    remove import

--- validate.py
+++ validate.py
@@ -5,7 +5,6 @@ import sys
 from pathlib import Path
 from typing import Sequence
 
-import rfc3987
 from jsonschema import Draft7Validator, FormatChecker
 from jsonschema.exceptions import ValidationError
 
